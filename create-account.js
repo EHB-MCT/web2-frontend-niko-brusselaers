749c0da72618ae90b85ad29b26271d89642cc3bf
niko.brusselaers <67846570+niko-brusselaers@users.noreply.github.com>
2021-12-14 00:00:40 +0100

    added login option

--- create-account.js
+++ create-account.js
@@ -2,15 +2,15 @@ const createAccount = document.getElementById("createAccount");
 createAccount.addEventListener('click', (event) => {
     event.preventDefault();
 
-    const username = document.getElementById('username').value.toString()
-    const password = document.getElementById('password').value.toString()
-    const passwordConfirm = document.getElementById('passwordConfirm').value.toString()
-    const email = document.getElementById('email').value.toString()
-    const firstname = document.getElementById('firstname').value.toString()
-    const lastname = document.getElementById('lastname').value.toString()
+    const username = document.getElementById('username').value.toString();
+    const password = document.getElementById('password').value.toString();
+    const passwordConfirm = document.getElementById('passwordConfirm').value.toString();
+    const email = document.getElementById('email').value.toString();
+    const firstname = document.getElementById('firstname').value.toString();
+    const lastname = document.getElementById('lastname').value.toString();
 
     if(password == passwordConfirm) {
-        fetch(" http://localhost:3000/create-account", {
+        fetch("http://localhost:3000/create-account", {
         method: "POST",
         headers:{'Content-Type': 'application/json',},
         body: JSON.stringify({
