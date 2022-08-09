fcb5dbc69104c630f403fbd62a1d9baa42fa3988
Niko.Brusselaers <niko.brusselaers@student.ehb.be>
2022-08-09 05:01:30 +0200

    updated login.js and created loginId.js, added comments to code
    updated login.js so that users are able to login and stay logged in, created loginId.js so when user change page, they stay logged in. and added comments

---
+++ docs/create-account.js
@@ -0,0 +1,41 @@
+const createAccount = document.getElementById("createAccountForm");
+createAccount.addEventListener('click', (event) => {
+    event.preventDefault();
+    //read all fields values
+    const username = document.getElementById('username').value.toString();
+    const password = document.getElementById('password').value.toString();
+    const passwordConfirm = document.getElementById('passwordConfirm').value.toString();
+    const email = document.getElementById('email').value.toString();
+    const firstname = document.getElementById('firstname').value.toString();
+    const lastname = document.getElementById('lastname').value.toString();
+    // if the password has been correctly typed twice, send request to rest api who checks if a account with same username or email already exists.
+    if (password == passwordConfirm) {
+        //https://web2-backend-niko-brusselaers.herokuapp.com
+        fetch(`http://localhost:3000//create-account`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "username": username,
+                    "password": password,
+                    "email": email,
+                    "firstname": firstname,
+                    "lastname": lastname
+                })
+            })
+            // if no user already exists with same username or email, api creates account and user gets rerouted to the login page
+            .then((response) => response.json())
+            .then(data => {
+                console.log(data);
+                window.location.replace("login.html");
+
+            }).catch((error) => {
+                console.error(error);
+            })
+    } else {
+        console.log({
+            error: "password doesnt match"
+        });
+    }
+})
---
+++ docs/login.js
@@ -0,0 +1,35 @@
+const login = document.getElementById('loginForm')
+login.addEventListener('submit', (event) => {
+    event.preventDefault();
+    //read all fields values and send the data to rest api
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const singedIn = document.getElementById('keepSignedIn').checked
+    //https://web2-backend-niko-brusselaers.herokuapp.com
+    fetch(`http://localhost:3000/login`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+        })
+        .then(response => response.json())
+        .then(response => {
+            // if successful, store the username and userId in  sessionStorage so the user stays logged in during current session. or localstorage when the user has checked keepSignedIn box.
+            console.log(response);
+            sessionStorage.setItem("id", response.id[0]);
+            sessionStorage.setItem('username', response.username);
+            if (singedIn) {
+                localStorage.setItem('id', response.id[0]);
+                localStorage.setItem('username', response.username);
+            }
+            // reroute to the main page after successful login
+            console.log(response);
+            window.location.replace("index.html");
+        }).catch((error) => {
+            console.log(error);
+        })
+})
